b17cfea0e9f894518e384227e2af37ce1636f2cb
Glaz-08 <130676103+Glaz-08@users.noreply.github.com>
2025-11-29 20:36:01 -0300

    feature correccion

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hola, soy manuel';
+    return 'Hola, soy manuel, hola hola';
   }
 }
